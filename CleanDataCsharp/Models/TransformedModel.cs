8802c659eb3fd1d359d231233ebedf0888a2a217
KevinDev98 <kevdan.devti@gmail.com>
2023-03-08 10:15:24 -0600

    delimitador 2

--- CleanDataCsharp/Models/TransformedModel.cs
+++ CleanDataCsharp/Models/TransformedModel.cs
@@ -7,7 +7,6 @@
         public string? ContenedorRejected { get; set; }
         //public string? ExtencionArchivosN { get; set; }
         public List<string>? NombresArchivosN { get; set; }
-        public string? delimitador { get; set; } 
         public string? usuarioemail { get; set; }
     }
 }
